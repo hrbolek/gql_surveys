ba2f73d5d8e720e68687745e1a905f32951249ee
Hrbolek <alexandr.stefek@unob.cz>
2024-08-25 21:18:21 +0200

    DEMODATA env better

--- src/DBFeeder.py
+++ src/DBFeeder.py
@@ -275,7 +275,7 @@ def get_demodata():
 
 async def initDB(asyncSessionMaker):
 
-    if os.environ.get("DEMODATA", None) == "False":
+    if os.environ.get("DEMODATA", None) in ["False", "false"]:
         dbModels = [
             QuestionTypeModel,
             SurveyTypeModel,
